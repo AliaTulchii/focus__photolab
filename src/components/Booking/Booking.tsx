112c46f08fd41280e8f903d833450fca44f57bd7
Halina Tulchii <halendeihalyna@gmail.com>
2024-07-22 18:59:49 +0300

    added  formattedSelectedHours for  selectedHours

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,3 +1,79 @@
+// import { useState } from "react";
+// import Button from "../Button/Button";
+// import RightModal from "../Modals/RightModal/RightModal";
+
+// const Booking = () => {
+//   const [showBooked, setShowBooked] = useState(false);
+//   const [selectedHours, setSelectedHours] = useState(0); // Використовуємо number замість string
+//   const [selectedPrice, setSelectedPrice] = useState(0); // Використовуємо number замість string
+
+//   const openModalBooked = (minutes: number, price: number) => {
+//     setShowBooked(true);
+//     setSelectedHours(minutes);
+//     setSelectedPrice(price);
+//   };
+
+//   const closeModal = () => {
+//     setShowBooked(false);
+//   };
+
+//   return (
+//     <div>
+//       <ul className="start__list">
+//         <li className="start__item">
+//           <Button
+//             type="button"
+//             className={"btn btn__booking"}
+//             text={"1 година - 1 грн"}
+//             onClick={() => openModalBooked(60, 1)}
+//           />
+//         </li>
+//         <li className="start__item">
+//           <Button
+//             type="button"
+//             className={"btn btn__booking"}
+//             text={"2 години - 2 грн"}
+//             onClick={() => openModalBooked(120, 2)}
+//           />
+//         </li>
+//         <li className="start__item">
+//           <Button
+//             type="button"
+//             className={"btn btn__booking"}
+//             text={"3 години - 3 грн"}
+//             onClick={() => openModalBooked(180, 3)}
+//           />
+//         </li>
+//         <li className="start__item">
+//           <Button
+//             type="button"
+//             className={"btn btn__booking"}
+//             text={"4 години - 4 грн"}
+//             onClick={() => openModalBooked(240, 4)}
+//           />
+//         </li>
+//         <li className="start__item">
+//           <Button
+//             type="button"
+//             className={"btn btn__booking"}
+//             text={"5 годин - 5 грн"}
+//             onClick={() => openModalBooked(300, 5)}
+//           />
+//         </li>
+//       </ul>
+//       <RightModal
+//         showBooked={showBooked}
+//         closeModal={closeModal}
+//         selectedHours={selectedHours}
+//         selectedPrice={selectedPrice}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Booking;
+
+
 import { useState } from "react";
 import Button from "../Button/Button";
 import RightModal from "../Modals/RightModal/RightModal";
@@ -7,9 +83,9 @@ const Booking = () => {
   const [selectedHours, setSelectedHours] = useState(0); // Використовуємо number замість string
   const [selectedPrice, setSelectedPrice] = useState(0); // Використовуємо number замість string
 
-  const openModalBooked = (minutes: number, price: number) => {
+  const openModalBooked = (hours: number, minutes: number, price: number) => {
     setShowBooked(true);
-    setSelectedHours(minutes);
+    setSelectedHours(hours * 60 + minutes); // Перетворення годин і хвилин в хвилини
     setSelectedPrice(price);
   };
 
@@ -25,7 +101,7 @@ const Booking = () => {
             type="button"
             className={"btn btn__booking"}
             text={"1 година - 1 грн"}
-            onClick={() => openModalBooked(80, 1)}
+            onClick={() => openModalBooked(1, 20, 1)}
           />
         </li>
         <li className="start__item">
@@ -33,7 +109,7 @@ const Booking = () => {
             type="button"
             className={"btn btn__booking"}
             text={"2 години - 2 грн"}
-            onClick={() => openModalBooked(120, 2)}
+            onClick={() => openModalBooked(2, 0, 2)}
           />
         </li>
         <li className="start__item">
@@ -41,7 +117,7 @@ const Booking = () => {
             type="button"
             className={"btn btn__booking"}
             text={"3 години - 3 грн"}
-            onClick={() => openModalBooked(180, 3)}
+            onClick={() => openModalBooked(3, 0, 3)}
           />
         </li>
         <li className="start__item">
@@ -49,7 +125,7 @@ const Booking = () => {
             type="button"
             className={"btn btn__booking"}
             text={"4 години - 4 грн"}
-            onClick={() => openModalBooked(240, 4)}
+            onClick={() => openModalBooked(4, 0, 4)}
           />
         </li>
         <li className="start__item">
@@ -57,7 +133,7 @@ const Booking = () => {
             type="button"
             className={"btn btn__booking"}
             text={"5 годин - 5 грн"}
-            onClick={() => openModalBooked(300, 5)}
+            onClick={() => openModalBooked(5, 0, 5)}
           />
         </li>
       </ul>
--- src/components/Booking/PaymentContent.tsx
+++ src/components/Booking/PaymentContent.tsx
@@ -1,3 +1,188 @@
+// import React from "react";
+// import { motion } from "framer-motion";
+// import { IoClose } from "react-icons/io5";
+// import "../../sass/components/_payment.scss";
+// import axios from "axios";
+// import { format } from "date-fns";
+// import { toDate } from "date-fns-tz";
+// import { useFormik } from "formik";
+// import * as Yup from "yup";
+
+// interface PaymentContentProps {
+//   closeModal: () => void;
+//   selectedData: Date | null;
+//   selectedSlot: { start: string; end: string } | null;
+//   selectedPrice: number;
+//   selectedHours: number;
+// }
+
+// const PaymentContent: React.FC<PaymentContentProps> = ({
+//   selectedData,
+//   selectedSlot,
+//   selectedPrice,
+//   selectedHours,
+//   closeModal,
+// }) => {
+//   const formik = useFormik({
+//     initialValues: {
+//       name: "",
+//       number: "",
+//       email: "",
+//     },
+//     validationSchema: Yup.object({
+//       name: Yup.string().required("Введіть ваше ім’я"),
+//       number: Yup.string()
+//         .matches(/^\+380\d{9}$/, "Номер телефону повинен містити 13 цифр і починатись +380")
+//         .required("Введіть номер телефону"),
+//       email: Yup.string().email("Неправильний формат e-mail").required("Введіть e-mail адресу"),
+//     }),
+//     onSubmit: async (values) => {
+//       if (!selectedData || !selectedSlot) {
+//         console.error("Missing booking date or slot");
+//         return;
+//       }
+
+//       const timeZone = "Europe/Kiev";
+//       const selectedStartDate = new Date(
+//         `${format(selectedData, "yyyy-MM-dd")}T${selectedSlot.start}:00`
+//       );
+
+//       const zonedDate = toDate(selectedStartDate, { timeZone });
+//       const bookingDate = zonedDate.toISOString();
+
+//       console.log("Selected start date:", selectedStartDate);
+//       console.log("Zoned date:", zonedDate);
+//       console.log("Booking date (ISO):", bookingDate);
+
+//       try {
+//         console.log("Submitting form...");
+//         console.log("Form data:", values);
+//         console.log("Selected data:", selectedData);
+//         console.log("Selected slot:", selectedSlot);
+//         console.log("Selected price:", selectedPrice);
+
+//         const response = await axios.post(
+//           `https://art-studio-api-production.up.railway.app/api/bookings/book?price=${selectedPrice}`,
+//           {
+//             customerName: values.name,
+//             customerPhone: values.number,
+//             customerEmail: values.email,
+//             bookingDate,
+//             bookingHours: selectedHours.toString(),
+//             productPrice: selectedPrice
+//           }
+//         );
+
+//         console.log("Response:", response);
+
+//         const booking = response.data.booking;
+//         const bookingId = booking._id;
+
+//         console.log("Booking request status:", response.status);
+//         console.log("Booking request response:", booking);
+
+//         setTimeout(async () => {
+//           console.log("Fetching payment URL...");
+//           const paymentResponse = await axios.get(
+//             `https://art-studio-api-production.up.railway.app/api/payments/payment-form?currency=UAH&productName[]=photosession&productCount[]=1&bookingId=${bookingId}`
+//           );
+
+//           console.log("Payment request status:", paymentResponse.status);
+//           console.log("Payment request response:", paymentResponse.data);
+
+//           if (paymentResponse.status === 200) {
+//             window.location.href = `https://art-studio-api-production.up.railway.app/api/payments/payment-form?currency=UAH&productName[]=photosession&productCount[]=1&bookingId=${bookingId}`;
+//           } else {
+//             console.error("Error in payment request:", paymentResponse.data);
+//           }
+//         }, 1000);
+//       } catch (error) {
+//         console.error("Error submitting the form", error);
+//       }
+//     },
+//   });
+
+//   return (
+//     <motion.div
+//       onClick={(e) => e.stopPropagation()}
+//       initial={{ y: "100%" }}
+//       animate={{ y: "0%" }}
+//       exit={{ y: "100%" }}
+//       transition={{ duration: 0.5 }}
+//       className="feedback__content container"
+//     >
+//       <div className="box__payment">
+//         <h1 className="title">Оплата</h1>
+//         <button className="payment__btn" onClick={closeModal}>
+//           <IoClose className="feedback__close" />
+//         </button>
+//       </div>
+
+//       <div className="box__booking">
+//         <h2 className="payment__subtitle">Бронювання:</h2>
+//         <p className="payment__text">
+//           {selectedData?.toLocaleDateString()}{" "}
+//           {selectedSlot ? `${selectedSlot.start}-${selectedSlot.end}` : ""}
+//         </p>
+//       </div>
+
+//       <div className="box__line" />
+
+//       <div className="feedback__formbox">
+//         <form action="" className="form" onSubmit={formik.handleSubmit}>
+//           <div className="form__inputs">
+//             <input
+//               name="name"
+//               value={formik.values.name}
+//               onChange={formik.handleChange}
+//               onBlur={formik.handleBlur}
+//               placeholder="Ваше ім’я"
+//               className="form__input form__name"
+//             />
+//             {formik.touched.name && formik.errors.name ? (
+//               <div className="form__error">{formik.errors.name}</div>
+//             ) : null}
+//             <input
+//               name="number"
+//               value={formik.values.number}
+//               onChange={formik.handleChange}
+//               onBlur={formik.handleBlur}
+//               placeholder="Номер телефону"
+//               className="form__input form__name"
+//             />
+//             {formik.touched.number && formik.errors.number ? (
+//               <div className="form__error">{formik.errors.number}</div>
+//             ) : null}
+//             <input
+//               name="email"
+//               value={formik.values.email}
+//               onChange={formik.handleChange}
+//               onBlur={formik.handleBlur}
+//               placeholder="E-mail адреса"
+//               className="form__input form__name"
+//             />
+//             {formik.touched.email && formik.errors.email ? (
+//               <div className="form__error">{formik.errors.email}</div>
+//             ) : null}
+//           </div>
+
+//           <div className="box__topay">
+//             <h2 className="payment__subtitle">До сплати:</h2>
+//             <p className="payment__text">{selectedPrice} грн</p>
+//           </div>
+
+//           <button type="submit" className="btn btn__booking">
+//             Сплатити
+//           </button>
+//         </form>
+//       </div>
+//     </motion.div>
+//   );
+// };
+
+// export default PaymentContent;
+
+
 import React from "react";
 import { motion } from "framer-motion";
 import { IoClose } from "react-icons/io5";
@@ -50,9 +235,15 @@ const PaymentContent: React.FC<PaymentContentProps> = ({
       const zonedDate = toDate(selectedStartDate, { timeZone });
       const bookingDate = zonedDate.toISOString();
 
+      // Перетворення selectedHours в формат годин і хвилин
+      const hours = Math.floor(selectedHours / 60);
+      const minutes = selectedHours % 60;
+      const formattedSelectedHours = `${hours}год ${minutes}мін`;
+
       console.log("Selected start date:", selectedStartDate);
       console.log("Zoned date:", zonedDate);
       console.log("Booking date (ISO):", bookingDate);
+      console.log("Selected hours (formatted):", formattedSelectedHours);
 
       try {
         console.log("Submitting form...");
@@ -68,7 +259,7 @@ const PaymentContent: React.FC<PaymentContentProps> = ({
             customerPhone: values.number,
             customerEmail: values.email,
             bookingDate,
-            bookingHours: selectedHours.toString(),
+            bookingHours: formattedSelectedHours.toString(), // Перевірте, що це значення передається у хвилинах
             productPrice: selectedPrice
           }
         );
@@ -181,5 +372,3 @@ const PaymentContent: React.FC<PaymentContentProps> = ({
 };
 
 export default PaymentContent;
-
-
