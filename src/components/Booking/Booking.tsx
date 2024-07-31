563bfce677e514a539fb222811048cfaf05c57be
Halina Tulchii <halendeihalyna@gmail.com>
2024-07-31 19:00:54 +0300

    changed prices

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,91 +1,15 @@
-// import { useState } from "react";
-// import Button from "../Button/Button";
-// import RightModal from "../Modals/RightModal/RightModal";
-
-// const Booking = () => {
-//   const [showBooked, setShowBooked] = useState(false);
-//   const [selectedHours, setSelectedHours] = useState(0); // Використовуємо number замість string
-//   const [selectedPrice, setSelectedPrice] = useState(0); // Використовуємо number замість string
-
-//   const openModalBooked = (minutes: number, price: number) => {
-//     setShowBooked(true);
-//     setSelectedHours(minutes);
-//     setSelectedPrice(price);
-//   };
-
-//   const closeModal = () => {
-//     setShowBooked(false);
-//   };
-
-//   return (
-//     <div>
-//       <ul className="start__list">
-//         <li className="start__item">
-//           <Button
-//             type="button"
-//             className={"btn btn__booking"}
-//             text={"1 година - 1 грн"}
-//             onClick={() => openModalBooked(60, 1)}
-//           />
-//         </li>
-//         <li className="start__item">
-//           <Button
-//             type="button"
-//             className={"btn btn__booking"}
-//             text={"2 години - 2 грн"}
-//             onClick={() => openModalBooked(120, 2)}
-//           />
-//         </li>
-//         <li className="start__item">
-//           <Button
-//             type="button"
-//             className={"btn btn__booking"}
-//             text={"3 години - 3 грн"}
-//             onClick={() => openModalBooked(180, 3)}
-//           />
-//         </li>
-//         <li className="start__item">
-//           <Button
-//             type="button"
-//             className={"btn btn__booking"}
-//             text={"4 години - 4 грн"}
-//             onClick={() => openModalBooked(240, 4)}
-//           />
-//         </li>
-//         <li className="start__item">
-//           <Button
-//             type="button"
-//             className={"btn btn__booking"}
-//             text={"5 годин - 5 грн"}
-//             onClick={() => openModalBooked(300, 5)}
-//           />
-//         </li>
-//       </ul>
-//       <RightModal
-//         showBooked={showBooked}
-//         closeModal={closeModal}
-//         selectedHours={selectedHours}
-//         selectedPrice={selectedPrice}
-//       />
-//     </div>
-//   );
-// };
-
-// export default Booking;
-
-
 import { useState } from "react";
 import Button from "../Button/Button";
 import RightModal from "../Modals/RightModal/RightModal";
 
 const Booking = () => {
   const [showBooked, setShowBooked] = useState(false);
-  const [selectedHours, setSelectedHours] = useState(0); // Використовуємо number замість string
-  const [selectedPrice, setSelectedPrice] = useState(0); // Використовуємо number замість string
+  const [selectedHours, setSelectedHours] = useState(0); 
+  const [selectedPrice, setSelectedPrice] = useState(0); 
 
   const openModalBooked = (hours: number, minutes: number, price: number) => {
     setShowBooked(true);
-    setSelectedHours(hours * 60 + minutes); // Перетворення годин і хвилин в хвилини
+    setSelectedHours(hours * 60 + minutes); 
     setSelectedPrice(price);
   };
 
@@ -100,40 +24,40 @@ const Booking = () => {
           <Button
             type="button"
             className={"btn btn__booking"}
-            text={"1 година - 1 грн"}
-            onClick={() => openModalBooked(1, 20, 1)}
+            text={"1 година - 800 грн"}
+            onClick={() => openModalBooked(1, 20, 800)}
           />
         </li>
         <li className="start__item">
           <Button
             type="button"
             className={"btn btn__booking"}
-            text={"2 години - 2 грн"}
-            onClick={() => openModalBooked(2, 0, 2)}
+            text={"2 години - 1600 грн"}
+            onClick={() => openModalBooked(2, 0, 1600)}
           />
         </li>
         <li className="start__item">
           <Button
             type="button"
             className={"btn btn__booking"}
-            text={"3 години - 3 грн"}
-            onClick={() => openModalBooked(3, 0, 3)}
+            text={"3 години - 2200 грн"}
+            onClick={() => openModalBooked(3, 0, 2200)}
           />
         </li>
         <li className="start__item">
           <Button
             type="button"
             className={"btn btn__booking"}
-            text={"4 години - 4 грн"}
-            onClick={() => openModalBooked(4, 0, 4)}
+            text={"4 години - 2900 грн"}
+            onClick={() => openModalBooked(4, 0, 2900)}
           />
         </li>
         <li className="start__item">
           <Button
             type="button"
             className={"btn btn__booking"}
-            text={"5 годин - 5 грн"}
-            onClick={() => openModalBooked(5, 0, 5)}
+            text={"5 годин - 3500 грн"}
+            onClick={() => openModalBooked(5, 0, 3500)}
           />
         </li>
       </ul>
